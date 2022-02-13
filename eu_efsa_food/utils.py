5b7e78904c5cac26eb883e5a3edb86c1632b15e4
Oracy Martos <ext.oracy.martos@farfetch.com>
2022-02-13 04:32:40 -0300

    Add Typing that was missing

--- eu_efsa_food/utils.py
+++ eu_efsa_food/utils.py
@@ -1,6 +1,8 @@
 import os
-from typing import List
+from typing import Any, Dict, List
 
+import pandas as pd
+from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.utils.task_group import TaskGroup
@@ -19,7 +21,7 @@ def get_load_table_names(files_path: str) -> List[str]:
         An array with all create tables. For example:
         ["acute_g_day_bw_all_days", "acute_g_day_bw_cons_days", "chronic_g_day_bw_to_t_pop", ...]
     """
-    load_table_names = []
+    load_table_names: List[str] = []
     for root, directories, files in os.walk(files_path):
         if "source" in root:
             for name in files:
@@ -37,7 +39,7 @@ def get_create_table_names(queries_path: str) -> List[str]:
         An array with all create tables. For example:
         ["acute_g_day_bw_all_days", "acute_g_day_bw_cons_days", "chronic_g_day_bw_to_t_pop", ...]
     """
-    create_table_names = []
+    create_table_names: List[str] = []
     for root, directories, files in os.walk(queries_path):
         if "create_tables" in root:
             for name in files:
@@ -46,7 +48,7 @@ def get_create_table_names(queries_path: str) -> List[str]:
 
 
 def create_tables_group_task(
-    create_table_names: List[str], create_tables_queries_path: str, dag
+    create_table_names: List[str], create_tables_queries_path: str, dag: DAG
 ) -> TaskGroup:
     """Create group task on flow with sql queries that create tables on database.
 
@@ -60,9 +62,9 @@ def create_tables_group_task(
         ["acute_g_day_bw_all_days", "acute_g_day_bw_cons_days", "chronic_g_day_bw_to_t_pop", ...]
     """
     with TaskGroup(group_id="create_table_group") as create_table_tasks_group:
-        create_table_tasks = []
+        create_table_tasks: List[PostgresOperator] = []
         for create_table in create_table_names:
-            last_insert_task = PostgresOperator(
+            last_insert_task: PostgresOperator = PostgresOperator(
                 task_id="create_table_" + create_table,
                 postgres_conn_id="postgres_data_source",
                 sql=f"{create_tables_queries_path}{create_table}.sql",
@@ -73,7 +75,7 @@ def create_tables_group_task(
     return create_table_tasks_group
 
 
-def load_data_group_task(load_table_names: List[str], files_path: str, dag) -> TaskGroup:
+def load_data_group_task(load_table_names: List[str], files_path: str, dag: DAG) -> "TaskGroup":
     """Create group task on flow with parquet files that will be loaded on database.
 
     Args:
@@ -86,9 +88,9 @@ def load_data_group_task(load_table_names: List[str], files_path: str, dag) -> T
         ["acute_g_day_bw_all_days", "acute_g_day_bw_cons_days", "chronic_g_day_bw_to_t_pop", ...]
     """
     with TaskGroup(group_id="load_data_group") as load_data_tasks_group:
-        load_data_tasks = []
+        load_data_tasks: List[PythonOperator] = []
         for table_name in load_table_names:
-            load_data_task = PythonOperator(
+            load_data_task: PythonOperator = PythonOperator(
                 task_id="load_data_" + table_name,
                 provide_context=True,
                 python_callable=write_parquet_to_postgres,
@@ -102,7 +104,7 @@ def load_data_group_task(load_table_names: List[str], files_path: str, dag) -> T
     return load_data_tasks_group
 
 
-def write_parquet_to_postgres(**kwargs) -> None:
+def write_parquet_to_postgres(**kwargs: Dict[str, Any]) -> None:
     """Insert data from parquet to postgres database using sqlalchemy create_engine.
 
     Args:
@@ -112,7 +114,7 @@ def write_parquet_to_postgres(**kwargs) -> None:
         Only a f-string with which table were inserted. For example:
         acute_g_day_bw_all_days Writed!
     """
-    table_name = kwargs.get("table_name")
-    df = read_parquet(**kwargs)
+    table_name: str = kwargs.get("table_name")
+    df: pd.DataFrame = read_parquet(**kwargs)
     engine = create_engine("postgresql://postgres:postgres@172.20.0.2:5432/postgres")
     df.to_sql(table_name, engine, if_exists="replace")
